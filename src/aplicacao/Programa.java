6c5a977d148f716a79f45326c1f051c2f018b321
Eribaldo Oliveira <desenvolvedorwebst@gmail.com>
2023-08-25 20:08:20 -0300

    Formatação do Layout

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -9,27 +9,32 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
-		double lA, lB, lX;
+		double lA, lB;
 		
-		System.out.println("================================");
+		System.out.println();
+		
+		System.out.println("Descubra qual a medida do lado C do triangulo:");
+		System.out.println("==============================================");
+		System.out.println();
 		System.out.println("Digite as medidas do triangulo: ");
-		System.out.println("================================");
+		//System.out.println("=====================================");
 		lA = sc.nextDouble();
 		lB = sc.nextDouble();
 		//xC = sc.nextDouble();
 		
-		double lx = 180 - (lA + lB);
-		
+		double lC = 180 - (lA + lB);
+	
 		//double  p = (xA + xB + xC) / 2.0;
 		//double areax = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
 		
+		
 		System.out.println("..................................");
-		System.out.printf("O lado X do triangulo é : [%.1fº]%n", lx);
+		System.out.printf("O lado C do triangulo é : [%.1fº]%n", lC);
 		System.out.println("..................................");
 		
 		System.out.printf("Lado A: [%.1fº]%n", lA);
 		System.out.printf("Lado B: [%.1fº]%n", lB);
-		System.out.printf("Lado X: [%.1fº]%n", lx);
+		System.out.printf("Lado C: [%.1fº]%n", lC);
 		
 		
 		sc.close();
